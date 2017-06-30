eb96b8a7730971b59b9e65027bad62a3926d3244
Gopi Krishna <srungarapugopikrishna@gmail.com>
2017-06-30 17:53:11 +0530

    updating models

--- myblog/models.py
+++ myblog/models.py
@@ -4,12 +4,19 @@ from datetime import datetime
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
 # Create your models here.
+class Genres(models.Model):
+	genre_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+	genre_name = models.CharField(max_length=50)
+# def __unicode__(self):
+# 	return self.genre_name
+
 class Story(models.Model):
 	story_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
 	#story_id = models.UUIDField(primary_key=True, editable=True)
 	title = models.CharField(max_length=500)
 	text = HTMLField()
-	genre_id = models.ForeignKey('Genres', default=uuid.uuid4)
+	#genre_id = models.ForeignKey(Genres, default=uuid.uuid4)
+	genre_id = models.ForeignKey(Genres, default=None)
 	created_by = models.ForeignKey(User, editable=True)
 	pub_date = models.DateTimeField(default=datetime.now,blank=True)
 	# def __unicode__(self):
@@ -21,10 +28,3 @@ class relations(models.Model):
 	child_id = models.CharField(max_length=500)
 	# def __unicode__(self):
 	# 	return str(self.relation_id)
-
-class Genres(models.Model):
-	genre_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-	genre_name = models.CharField(max_length=50)
-
-	# def __unicode__(self):
-	# 	return self.genre_name
