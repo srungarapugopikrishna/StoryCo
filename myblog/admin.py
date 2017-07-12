ec095d034b26cc88be48f867f39d5683b3be0441
Gopi Krishna <srungarapugopikrishna@gmail.com>
2017-07-12 15:52:40 +0530

    registered newly added models

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Story,relations,Genres
+from .models import Story,relations,Genres,Category,Episode,Relationship,Item
 
 # Register your models here.
 admin.site.register(Story)
 admin.site.register(relations)
 admin.site.register(Genres)
+admin.site.register(Category)
+admin.site.register(Episode)
+admin.site.register(Relationship)
+admin.site.register(Item)
+
