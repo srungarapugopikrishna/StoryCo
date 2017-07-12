598d0dc96e2792c3346bd07ff14a9ac40bc20ab3
Gopi Krishna <srungarapugopikrishna@gmail.com>
2017-07-12 15:49:32 +0530

    removed someform url

--- myblog/urls.py
+++ myblog/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
 	url(r'^home$', views.home, name='home'),
 	url(r'^editor$', views.editor, name='editor'),
 	url(r'^(?P<s_id>.*)/editor_child/$', views.editor_child, name='editorChild'	),
-	url(r'^some_view$', views.some_view, name='some_view'),
 	url(r'^(?P<s_id>.*)/StoryPage/$', views.StoryPage, name='StoryPage'),
 	url(r'^(?P<s_id>.*)/visualize/$', views.visualize, name='visualize'	),
 	url(r'^get_data$', views.get_data, name='get_data'),
