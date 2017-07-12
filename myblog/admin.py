a85e9b351acb7811c3d5a293ad267d042bebc5e8
Gopi Krishna <srungarapugopikrishna@gmail.com>
2017-07-12 17:24:02 +0530

    registering new models

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Story,relations,Genres,Category,Episode,Relationship,Item
+from .models import Story,relations,Genres,Category,Episode,Relationship,Item,Representation_Type,Content_Type
 
 # Register your models here.
 admin.site.register(Story)
@@ -9,4 +9,6 @@ admin.site.register(Category)
 admin.site.register(Episode)
 admin.site.register(Relationship)
 admin.site.register(Item)
+admin.site.register(Content_Type)
+admin.site.register(Representation_Type)
 
