f38c71cea3eb06f91464f4bcdc4fedcac3bdf499
Gopi Krishna <srungarapugopikrishna@gmail.com>
2017-07-18 19:25:32 +0530

    updating forms

--- myblog/forms.py
+++ myblog/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Story
+from .models import Story,Item
 
 class MainForm(forms.ModelForm):
 
     class Meta:
         model = Story
         fields = ('title', 'text', 'genre_id')
+
+class ItemForm(forms.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('item_title', 'item_description')
