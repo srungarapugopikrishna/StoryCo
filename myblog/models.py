7e996ce729c89839431bc72c3733d33816c5369d
Gopi Krishna <srungarapugopikrishna@gmail.com>
2017-08-02 19:19:38 +0530

    updating models

--- myblog/models.py
+++ myblog/models.py
@@ -25,8 +25,8 @@ class relations(models.Model):
 	relation_id = 	models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
 	parent_id 	= 	models.CharField(max_length=500)
 	child_id 	= 	models.CharField(max_length=500)
-	# def __unicode__(self):
-	# 	return str(self.relation_id)
+	def __unicode__(self):
+		return self.relation_id
 
 
 
@@ -34,6 +34,8 @@ class Category(models.Model):
 	category_id 			= 	models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
 	category_label 			= 	models.CharField(max_length=50)
 	category_description 	= 	models.CharField(max_length=500)
+	def __unicode__(self):
+		return self.category_label
 
 class Content_Type(models.Model):
 	content_id 		= 	models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
@@ -52,7 +54,7 @@ class Representation_Type(models.Model):
 class Item(models.Model):
 	item_id 			= 	models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
 	item_title 			=	models.CharField(max_length=500)
-	item_type = models.ForeignKey(Representation_Type, default=None, null=True)
+	item_type 			=	models.ForeignKey(Representation_Type, default=None, null=True)
 	item_description	=	models.CharField(max_length=1000)
 	created_by 			= 	models.ForeignKey(User, editable=True)
 	pub_date			=	models.DateTimeField(default=datetime.now,blank=True)
@@ -64,7 +66,8 @@ class Main_Item(models.Model):
 	mainItem_description = models.CharField(max_length=500)
 	created_by = models.ForeignKey(User, editable=True)
 	pub_date = models.DateTimeField(default=datetime.now,blank=True)
-
+	def __unicode__(self):
+		return self.mainItem_title
 
 class Episode(models.Model):
 	episode_id 						= 	models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
