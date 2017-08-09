337bfd406cc0dab8babae2a52c1c76d37680c77a
srungarapugopikrishna <srungarapugopikrishna@gmail.com>
2017-08-09 08:29:25 +0530

    updating admin with modified model names

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Story,relations,Genres,Category,Episode,Relationship,Item,Representation_Type,Content_Type
+from .models import Story,Relations,Genres,Category,Episode,Relationship,Item,RepresentationType,ContentType
 
 # Register your models here.
 admin.site.register(Story)
-admin.site.register(relations)
+admin.site.register(Relations)
 admin.site.register(Genres)
 admin.site.register(Category)
 admin.site.register(Episode)
 admin.site.register(Relationship)
 admin.site.register(Item)
-admin.site.register(Content_Type)
-admin.site.register(Representation_Type)
+admin.site.register(ContentType)
+admin.site.register(RepresentationType)
