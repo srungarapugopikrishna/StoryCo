e23400d2d787f8bedde5dacebadb29d958107546
Gopi Krishna <srungarapugopikrishna@gmail.com>
2017-08-21 20:45:25 +0530

    Updating admin

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Story,Relations,Genres,Category,Episode,Relationship,Item,Represent,Content
+from .models import Story,relations,Genres,Category,Episode,Relationship,Item,Representation_Type,Content_Type
 
 #Register your models here.
 admin.site.register(Story)
-admin.site.register(Relations)
+admin.site.register(relations)
 admin.site.register(Genres)
 admin.site.register(Category)
 admin.site.register(Episode)
 admin.site.register(Relationship)
 admin.site.register(Item)
-admin.site.register(Content)
-admin.site.register(Represent)
+admin.site.register(Content_Type)
+admin.site.register(Representation_Type)
